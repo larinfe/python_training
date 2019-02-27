930ece0b220a8c8e2e85b81ac2d7769843d42877
Fedor Larin <larinfe@gmail.com>
2019-02-27 14:56:26 +0300

    Сделаны все тесты и набор вспомогательных методов для создания и удаления контактов и групп.

--- fixture/application.py
+++ fixture/application.py
@@ -13,30 +13,6 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
-    #
-    # def open_group_page(self):
-    #     wd = self.wd
-    #     wd.find_element_by_name("new").click()
-    #
-    # def submit_group_creation(self):
-    #     wd = self.wd
-    #     wd.find_element_by_name("submit").click()
-    #
-    # def fill_group_form(self, group):
-    #     wd = self.wd
-    #     self.open_group_page()
-    #     wd.find_element_by_name("group_name").send_keys(group.name)
-    #     wd.find_element_by_name("group_header").send_keys(group.header)
-    #     wd.find_element_by_name("group_footer").send_keys(group.footer)
-    #     wd.find_element_by_name("submit").click()
-    #
-    #
-    #
-    # def return_to_group_creation(self):
-    #     wd = self.wd
-    #     self.return_to_group_creation()
-    #     wd.find_element_by_link_text("group page").click()
-
 
 
     def destroy(self):
--- fixture/group.py
+++ fixture/group.py
@@ -18,10 +18,19 @@ class GroupHelper:
     def create(self, group):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
+        # init group creation
         self.open_group_page()
+        # fill group  from
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
 
