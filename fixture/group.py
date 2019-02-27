4fd8f64002aba8482d8f25c64f979f183a4e9442
Fedor Larin <larinfe@gmail.com>
2019-02-27 19:51:04 +0300

    Добавлен тест модификации групп.

--- fixture/group.py
+++ fixture/group.py
@@ -20,19 +20,33 @@ class GroupHelper:
         wd.find_element_by_link_text("groups").click()
         # init group creation
         self.open_group_page()
-        # fill group  from
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+        # wd.find_element_by_name("group_header").click()
+        # wd.find_element_by_name("group_header").clear()
+        # wd.find_element_by_name("group_header").send_keys(group.header)
+        #
+        # wd.find_element_by_name("group_footer").click()
+        # wd.find_element_by_name("group_footer").clear()
+        # wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+
 
     def return_to_group_creation(self):
         wd = self.app.wd
@@ -43,38 +57,52 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
         self.open_home_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group(wd)
         # submit deletion
         wd.find_element_by_name("delete").click()
         wd.find_element_by_link_text("group page").click()
 
+    def select_first_group(self, wd):
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+
     def submit_creation(self):
-            # submit contact creation
-            wd = self.app.wd
-            wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        # submit contact creation
+        wd = self.app.wd
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def fill_contact_form(self, contact):
-            # fill contact form
-            wd = self.app.wd
-            self.init_contact_creation()
-            wd.find_element_by_name("firstname").send_keys(contact.firstname)
-            wd.find_element_by_name("middlename").send_keys(contact.middlename)
-            wd.find_element_by_name("lastname").send_keys(contact.lastname)
-            wd.find_element_by_name("address").send_keys(contact.address)
-            wd.find_element_by_name("home").send_keys(contact.home)
-            self.submit_creation()
+        # fill contact form
+        wd = self.app.wd
+        self.init_contact_creation()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").send_keys(contact.home)
+        self.submit_creation()
 
     def init_contact_creation(self):
-            # init contact creation
-            wd = self.app.wd
-            wd.find_element_by_link_text("add new").click()
+        # init contact creation
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
 
     def delete_all_contact(self):
-            wd = self.app.wd
-            # select first group
-            wd.get("http://localhost/addressbook/")
-            wd.find_element_by_id("MassCB").click()
-            wd.find_element_by_xpath("//input[@value='Delete']").click()
-            # submit deletion
-            wd.switch_to_alert().accept()
+        wd = self.app.wd
+        # select first group
+        wd.get("http://localhost/addressbook/")
+        wd.find_element_by_id("MassCB").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        # submit deletion
+        wd.switch_to_alert().accept()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+        self.select_first_group(wd)
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.fill_group_form(new_group_data)
+        # submit
+        wd.find_element_by_name("update").click()
