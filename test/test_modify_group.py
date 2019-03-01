411fd1e7675c5db20445ddd06615b2c7638c278b
Fedor Larin <larinfe@gmail.com>
2019-03-01 19:52:33 +0300

    Тесты для удаления и модификации доработы предусловиями.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,10 +3,10 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(footer="list"))
-    app.group.modify_first_group(Group(name="new group"))
+    app.group.modify_first_group(Group(name="new grou2p"))
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
-        app.group.create(Group(name="beast"))
+        app.group.create(Group(name="beas2t"))
     app.group.modify_first_group(Group(header="new group2"))
