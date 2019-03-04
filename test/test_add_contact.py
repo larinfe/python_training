32f9b2ab8ab4cb21c0a88dc5a893b90f529e6523
Fedor Larin <larinfe@gmail.com>
2019-03-04 16:18:56 +0300

    выделена проверка равенства объектов и списков для групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -9,7 +9,7 @@ def test_add_contact(app):
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
 
-# def test_add_contact_empty(app):
-#     app.contact.fill_contact_form(Contact(firstname="", middlename="", lastname="", address="", home=""))
+def test_add_contact_empty(app):
+    app.contact.fill_contact_form(Contact(firstname="", middlename="", lastname="", address="", home=""))
 
 
